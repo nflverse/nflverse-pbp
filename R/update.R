6ed2b187413d598ea962daee0ba1db4950a0098c
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-04-25 15:32:00 -0400

    Updloading pbp

---
+++ R/update.R
@@ -0,0 +1,46 @@
+library(nflfastR)
+library(tidyverse)
+
+## STEP 1: SCRAPE OLD SEASONS
+write_season <- function(y) {
+  message(glue::glue('Year {y}: scraping play-by-play'))
+  
+  #get reg and post games
+  pbp <- fast_scraper_schedules(y) %>%
+    filter(game_type == "REG" | game_type == "POST") %>%
+    pull(game_id) %>%
+    fast_scraper(pp = TRUE)
+  
+  message(glue::glue('Year {y}: writing to file'))
+  write_csv(pbp, glue::glue('data/play_by_play_{y}.csv'))
+  saveRDS(pbp, glue::glue('data/play_by_play_{y}.rds'))
+}
+
+lapply(2000:2019, write_season)
+
+
+#read_csv(glue::glue('data/play_by_play_{y}.csv'))
+#readRDS(glue::glue('data/play_by_play_{y}.rds'))
+
+## STEP 2: SCRAPE ONGOING SEASON
+
+y = 2020
+
+#get reg and post games
+sched <- fast_scraper_schedules(y) %>%
+  filter(game_type == "REG" | game_type == "POST") %>%
+  select(game_id, week, game_type)
+
+pbp <- sched %>% pull(game_id) %>%
+  fast_scraper(source = 'gc', pp = TRUE) %>%
+  mutate(game_id = as.numeric(game_id)) %>%
+  #need to do this because week & game_type aren't in gc data
+  left_join(sched, by = "game_id")
+
+write_csv(pbp, glue::glue('data/play_by_play_{y}.csv'))
+saveRDS(pbp, glue::glue('data/play_by_play_{y}.rds'))
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nflfastR-data
-Data scraped from nflfastR package
+NFL play-by-play data scraped from the [`nflfastR` package](https://github.com/mrcaseb/nflfastR) going back to 2000. Each season contains both regular season and postseason data, with game_type denoting which.
+
+Data are stored in the data folder, available as either .csv or .rds.
