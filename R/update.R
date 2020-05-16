04baa6631e7d7a1520b44d96b6f6d49abaed421b
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-05-16 14:11:21 -0400

    Update function that builds cleaned data

--- R/update.R
+++ R/update.R
@@ -1,7 +1,8 @@
+source('R/scraping_functions.R')
+
 #devtools::install_github("mrcaseb/nflfastR")
 
 library(nflfastR)
-library(tidyverse)
 library(nflscrapR)
 
 ## STEP 1: SCRAPE OLD SEASONS
@@ -28,7 +29,8 @@ write_season <- function(y) {
   saveRDS(pbp, glue::glue('data/play_by_play_{y}.rds'))
 }
 
-nothing_in_here <- lapply(2000:2019, write_season)
+#don't leave this uncommented as this is probably going to be run periodically
+#nothing_in_here <- lapply(2000:2019, write_season)
 
 
 ## STEP 2: SCRAPE ONGOING SEASON
@@ -48,9 +50,18 @@ pbp <- sched %>% pull(game_id) %>%
 write_csv(pbp, glue::glue('data/play_by_play_{y}.csv.gz'))
 saveRDS(pbp, glue::glue('data/play_by_play_{y}.rds'))
 
+data_repo <- git2r::repository('./') # Set up connection to repository folder
+git2r::add(data_repo, 'data/*') # add specific files to staging of commit
+git2r::commit(data_repo, message = glue::glue("Updating data at {Sys.time()}")) # commit the staged files with the chosen message
+git2r::pull(data_repo) # pull repo (and pray there are no merge commits)
+git2r::push(data_repo, credentials = git2r::cred_user_pass(username = 'guga31bb', password = paste(password))) # push commit
+
+message(paste('Successfully uploaded to GitHub values as of',Sys.time())) # I have cron set up to pipe this message to healthchecks.io so that I can keep track if something is broken
+
 
 ## STEP 3: SCRAPE ROSTER
+#commenting out for now because it's broken
 
-roster <- teams_colors_logos %>% pull(team_id) %>% fast_scraper_roster(1999:2019, TRUE)
-write_csv(roster, glue::glue('roster-data/roster.csv.gz'))
-saveRDS(roster, glue::glue('roster-data/roster.rds'))
+# roster <- teams_colors_logos %>% pull(team_id) %>% fast_scraper_roster(1999:2019, TRUE)
+# write_csv(roster, glue::glue('roster-data/roster.csv.gz'))
+# saveRDS(roster, glue::glue('roster-data/roster.rds'))
