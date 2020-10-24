8bd9e58586bd579e00bc56e10e1dd2ed34cc3062
mrcaseb <roemerle@gmail.com>
2020-10-24 21:26:05 +0200

    Add new script to update all seasons

---
+++ R/update_all_seasons_pbp.R
@@ -0,0 +1,12 @@
+source("R/save_pbp.R")
+
+y <- dplyr::if_else(
+  lubridate::month(lubridate::today("America/New_York")) >= 9,
+  lubridate::year(lubridate::today("America/New_York")) ,
+  lubridate::year(lubridate::today("America/New_York")) - 1
+)
+
+purrr::walk(1999:y, save_pbp)
+
+# Any change in this script will trigger redownload of all seasons
+# starting in 1999
