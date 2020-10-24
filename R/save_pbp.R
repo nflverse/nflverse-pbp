93c2ed73df8bfedc5b64671d13018a623e31a0c7
mrcaseb <roemerle@gmail.com>
2020-10-24 21:19:18 +0200

    New save pbp function

---
+++ R/save_pbp.R
@@ -0,0 +1,20 @@
+library(dplyr)
+save_pbp <- function(season) {
+  ids <- fast_scraper_schedules(season) %>%
+    dplyr::filter(!is.na(home_result)) %>%
+    dplyr::pull(game_id)
+
+  pbp <- nflfastR::build_nflfastR_pbp(ids, pp = TRUE)
+
+  # rds
+  saveRDS(pbp, glue::glue('data/play_by_play_{y}.rds'))
+  # csv.gz
+  readr::write_csv(pbp, glue::glue('data/play_by_play_{y}.csv.gz'))
+  # .parquet
+  arrow::write_parquet(pbp, glue::glue('data/play_by_play_{y}.parquet'))
+  # .zip
+  readr::write_csv(pbp, glue::glue("data/play_by_play_{y}.csv"))
+  utils::zip(glue::glue("data/play_by_play_{y}.zip"), c(glue::glue("data/play_by_play_{y}.csv")))
+  file.remove(glue::glue("data/play_by_play_{y}.csv"))
+  return(invisible())
+}
