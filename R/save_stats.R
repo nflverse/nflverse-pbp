9fbdcbe9cfbfddbf499b0ec007ab7e7de855c0ee
mrcaseb <roemerle@gmail.com>
2021-02-18 22:26:22 +0100

    Add stats script and run it in the action

---
+++ R/save_stats.R
@@ -0,0 +1,28 @@
+`%>%` <- magrittr::`%>%`
+future::plan("multisession")
+pbp_df <- nflfastR::load_pbp(1999:nflfastR:::most_recent_season(), qs = TRUE)
+stats_df <- nflfastR::calculate_player_stats(pbp_df, weekly = TRUE)
+
+# rds
+saveRDS(stats_df, 'data/player_stats.rds')
+# csv.gz
+readr::write_csv(stats_df, 'data/player_stats.csv.gz')
+# .parquet
+arrow::write_parquet(stats_df, 'data/player_stats.parquet')
+# .qs
+qs::qsave(stats_df, 'data/player_stats.qs',
+          preset = "custom",
+          algorithm = "zstd_stream",
+          compress_level = 22,
+          shuffle_control = 15)
+
+message <- sprintf("Updated %s (ET) using nflfastR version %s", lubridate::now("America/New_York"), utils::packageVersion("nflfastR"))
+
+git <- function(..., echo_cmd = TRUE, echo = TRUE, error_on_status = FALSE) {
+  callr::run("git", c(...),
+             echo_cmd = echo_cmd, echo = echo,
+             error_on_status = error_on_status
+  )
+}
+
+git("commit", "-am", message)
