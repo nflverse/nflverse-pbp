96e9ed38bb245b7f4d454db5dfa332d4b92b7a72
Tan-DESKPC <tan@tanho.ca>
2022-02-12 13:50:17 -0500

    add timestamps to saving and archive old scripts

--- R/update_current_season_pbp.R
+++ R/update_current_season_pbp.R
@@ -1,12 +1,5 @@
 source("R/save_pbp.R")
 
-y <- dplyr::if_else(
-  lubridate::month(lubridate::today("America/New_York")) >= 9,
-  lubridate::year(lubridate::today("America/New_York")) ,
-  lubridate::year(lubridate::today("America/New_York")) - 1
-)
-
-future::plan("multisession")
-save_pbp(y)
+save_pbp(nflreadr::most_recent_season())
 
 list.files("data", pattern = paste0("play_by_play_",y), full.names = TRUE) |> nflversedata::nflverse_upload("pbp")
