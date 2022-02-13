12c0621a9037b7340e044a05c87ff69626fd1e86
Tan Ho <38083823+tanho63@users.noreply.github.com>
2022-02-13 13:46:53 -0500

    Update update_current_season_pbp.R

--- R/update_current_season_pbp.R
+++ R/update_current_season_pbp.R
@@ -1,5 +1,7 @@
 source("R/save_pbp.R")
 future::plan(future::multisession)
-save_pbp(nflreadr::most_recent_season())
+x <- nflreadr::most_recent_season()
+save_pbp(x)
 
-list.files("data", pattern = paste0("play_by_play_",y), full.names = TRUE) |> nflversedata::nflverse_upload("pbp")
+list.files("data", pattern = paste0("play_by_play_",x), full.names = TRUE) |> 
+  nflversedata::nflverse_upload("pbp")
