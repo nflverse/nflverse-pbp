3e43545471aa7fc06a62409c6f8f7e516c87fa22
Tan Ho <38083823+tanho63@users.noreply.github.com>
2022-02-13 13:10:10 -0500

    add back parallelize

--- R/update_all_seasons_pbp.R
+++ R/update_all_seasons_pbp.R
@@ -1,5 +1,5 @@
 source("R/save_pbp.R")
-
+future::plan(future::multisession)
 purrr::walk(1999:nflreadr::most_recent_season(), save_pbp)
 
 list.files("data", pattern = "play_by_play", full.names = TRUE) |>
