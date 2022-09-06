987900926ab9bd633aca86fec240f74ee001a48a
mrcaseb <38586519+mrcaseb@users.noreply.github.com>
2022-09-06 16:53:44 +0200

    Multiple laterals data (#63)
    * initial data and script for multiple lateral yards
    
    * rename laterals script and monitor changes for release
    
    * folder name has changed
    
    * use waldo to check if data has updated and make release in same workflow

---
+++ R/update_multiple_laterals.R
@@ -0,0 +1,86 @@
+save_lateral_yards <- function(s) {
+  future::plan("multisession")
+
+  games <- nflreadr::load_schedules(s) |>
+    dplyr::filter(!is.na(result))
+
+  if(length(games$game_id) == 0){
+    cli::cli_alert_info("No games yet, nothing to do")
+    return(invisible(NULL))
+  }
+
+  cli::cli_alert_info("Load {.val {length(games$game_id)}} game{?s} of {.val {s}}")
+
+  load <- furrr::future_map_dfr(games$game_id, check_lateral_yards)
+
+  cli::cli_alert_info("Process raw data...")
+
+  old <- readr::read_csv("lateral_yards/multiple_lateral_yards.csv", show_col_types = FALSE)
+
+  all <-
+    dplyr::bind_rows(
+      old,
+      load |>
+        nflfastR::decode_player_ids() |>
+        dplyr::group_by(game_id, play_id) |>
+        dplyr::filter(dplyr::n() > 1) |>
+        dplyr::ungroup() |>
+        suppressMessages()
+    ) |>
+    dplyr::distinct() |>
+    dplyr::arrange(game_id, play_id)
+
+  cli::cli_alert_info("Save multiple lateral plays...")
+
+  readr::write_csv(all, "lateral_yards/multiple_lateral_yards.csv")
+  future::plan("sequential")
+
+  # check if file has changed to trigger nflverse release
+  comp <- waldo::compare(old, all)
+
+  if (length(comp) != 0){
+    nflversedata::nflverse_save(
+      data_frame = all,
+      file_name = "multiple_lateral_yards",
+      nflverse_type = "multiple_lateral_yards",
+      release_tag = "misc",
+      file_types = c("rds", "csv")
+    )
+  } else {
+    cli::cli_alert_info("Data has not changed.")
+  }
+
+  cli::cli_alert_info("DONE!")
+  return(invisible(all))
+}
+
+check_lateral_yards <- function(id){
+  cli::cli_progress_step("Loading {id}")
+  season <- substr(id, 1, 4)
+  base_url <- "https://github.com/nflverse/nflfastR-raw/raw/master/raw"
+  load_url <- file.path(base_url, season, paste0(id, ".rds"))
+  raw_data <- nflreadr::rds_from_url(load_url)
+  plays <- janitor::clean_names(raw_data[[1]][[1]]$gameDetail$plays) |>
+    dplyr::select(play_id, play_stats)
+  stats <- tidyr::unnest(plays, cols = c("play_stats")) |>
+    janitor::clean_names() |>
+    dplyr::filter(stat_id %in% c(12, 13, 23, 24)) |>
+    dplyr::mutate(
+      game_id = as.character(id),
+      type = dplyr::if_else(stat_id %in% 12:13, "lateral_rushing", "lateral_receiving", missing = NA_character_)
+    ) |>
+    dplyr::select(
+      "game_id",
+      "play_id",
+      "stat_id",
+      "type",
+      "yards",
+      "team_abbr" = "team_abbreviation",
+      "player_name",
+      "gsis_player_id"
+    )
+
+  return(stats)
+}
+
+save_lateral_yards(nflreadr::most_recent_season())
