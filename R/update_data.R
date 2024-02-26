abffba4761e67ae83597ae02403846e101fc3cba
Sebastian Carl <mrcaseb@gmail.com>
2024-02-26 18:09:52 +0100

    multisession

--- R/update_data.R
+++ R/update_data.R
@@ -1,6 +1,9 @@
 season <- Sys.getenv("NFLVERSE_UPDATE_SEASON", unset = NA_character_) |> as.integer()
 type <- Sys.getenv("NFLVERSE_UPDATE_TYPE", unset = NA_character_)
 
+# Run parallel
+future::plan(future::multisession)
+
 if (any(is.na(season), is.na(type))) {
   cli::cli_abort("Can't find season {.val {season}} or type {.val {type}}")
 } else if (type == "pbp") {
