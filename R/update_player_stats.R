29ec73e774f108a865f7e9d7c45e2dbe5e205095
mrcaseb <38586519+mrcaseb@users.noreply.github.com>
2024-08-12 18:55:03 +0200

    Season level stats (#94)
    * season level offense stats
    
    * season level def stats
    
    * season level kicking stats
    
    * fix incorrect object names
    
    * more incorrect object names
    
    * allow public rspm and add nflverse dev repo
    to install dev versions of nflverse packages
    
    * renaming objects is so stupid smh
    
    * We have to add the season manually
    as nflfastR doesn't do this by default

--- R/update_player_stats.R
+++ R/update_player_stats.R
@@ -4,15 +4,62 @@
 release_playerstats_offense <- function(season){
   cli::cli_progress_step("Starting {.fct nflfastR::calculate_player_stats} for {season}!")
 
-  ps <- nflreadr::load_pbp(season) |>
-    nflfastR::calculate_player_stats(weekly = TRUE)
+  pbp <- nflreadr::load_pbp(season)
 
-  attr(ps, "nflfastR_version") <- packageVersion("nflfastR")
+  # WEEK LEVEL SUMMARY #########################################################
+
+  ps_weekly <- pbp |>
+    nflfastR::calculate_player_stats(weekly = TRUE)
+  attr(ps_weekly, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # SEASON LEVEL SUMMARY #######################################################
+
+  ps_reg_season <- pbp |>
+    dplyr::filter(season_type == "REG") |>
+    nflfastR::calculate_player_stats(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_post_season <- pbp |>
+    dplyr::filter(season_type == "POST") |>
+    nflfastR::calculate_player_stats(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_all_season <- pbp |>
+    nflfastR::calculate_player_stats(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG+POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_season <- dplyr::bind_rows(ps_reg_season, ps_post_season, ps_all_season)
+
+  attr(ps_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    data_frame = ps,
+    data_frame = ps_weekly,
     file_name =  glue::glue("player_stats_{season}"),
     nflverse_type = "player stats: offense",
+    release_tag = "player_stats",
+    file_types = c("rds","csv","parquet","qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = ps_season,
+    file_name =  glue::glue("player_stats_season_{season}"),
+    nflverse_type = "player stats: offense (season)",
     release_tag = "player_stats",
     file_types = c("rds","csv","parquet","qs", "csv.gz")
   )
@@ -23,19 +70,44 @@ release_playerstats_offense <- function(season){
 
 release_playerstats_offense_combined <- function(...){
   cli::cli_progress_step("Release combined offensive player stats...")
-  stats_df <-
+
+  # COMBINE WEEK LEVEL STATS ###################################################
+
+  stats_df_week <-
     purrr::map(
       seq(1999, nflreadr::most_recent_season()),
       ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_{.x}.qs"))
     ) |>
     purrr::list_rbind()
 
-  attr(stats_df, "nflfastR_version") <- packageVersion("nflfastR")
+  attr(stats_df_week, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # COMBINE SEASON LEVEL STATS #################################################
+
+  stats_df_season <-
+    purrr::map(
+      seq(1999, nflreadr::most_recent_season()),
+      ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_season_{.x}.qs"))
+    ) |>
+    purrr::list_rbind()
+
+  attr(stats_df_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    data_frame = stats_df,
+    data_frame = stats_df_week,
     file_name =  "player_stats",
     nflverse_type = "player stats: offense",
+    release_tag = "player_stats",
+    file_types = c("rds","csv","parquet","qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = stats_df_season,
+    file_name =  "player_stats_season",
+    nflverse_type = "player stats: offense (season)",
     release_tag = "player_stats",
     file_types = c("rds","csv","parquet","qs", "csv.gz")
   )
@@ -51,17 +123,64 @@ release_playerstats_offense_combined <- function(...){
 release_playerstats_defense <- function(season) {
   cli::cli_progress_step("Starting {.fct nflfastR::calculate_player_stats_def} for {season}!")
 
-  ps <- nflreadr::load_pbp(season) |>
-    nflfastR::calculate_player_stats_def(weekly = TRUE)
+  pbp <- nflreadr::load_pbp(season)
 
-  attr(ps, "nflfastR_version") <- packageVersion("nflfastR")
+  # WEEK LEVEL SUMMARY #########################################################
+
+  ps_weekly <- pbp |>
+    nflfastR::calculate_player_stats_def(weekly = TRUE)
+  attr(ps_weekly, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # SEASON LEVEL SUMMARY #######################################################
+
+  ps_reg_season <- pbp |>
+    dplyr::filter(season_type == "REG") |>
+    nflfastR::calculate_player_stats_def(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_post_season <- pbp |>
+    dplyr::filter(season_type == "POST") |>
+    nflfastR::calculate_player_stats_def(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_all_season <- pbp |>
+    nflfastR::calculate_player_stats_def(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG+POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_season <- dplyr::bind_rows(ps_reg_season, ps_post_season, ps_all_season)
+
+  attr(ps_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    data_frame = ps,
+    data_frame = ps_weekly,
     file_name = glue::glue("player_stats_def_{season}"),
     nflverse_type = "player stats: defense",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = ps_season,
+    file_name =  glue::glue("player_stats_def_season_{season}"),
+    nflverse_type = "player stats: defense (season)",
+    release_tag = "player_stats",
+    file_types = c("rds","csv","parquet","qs", "csv.gz")
   )
 
   cli::cli_progress_done()
@@ -70,19 +189,44 @@ release_playerstats_defense <- function(season) {
 
 release_playerstats_defense_combined <- function(...){
   cli::cli_progress_step("Release combined defensive player stats...")
-  stats_df <-
+
+  # COMBINE WEEK LEVEL STATS ###################################################
+
+  stats_df_week <-
     purrr::map(
       seq(1999, nflreadr::most_recent_season()),
       ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_def_{.x}.qs"))
     ) |>
     purrr::list_rbind()
 
-  attr(stats_df, "nflfastR_version") <- packageVersion("nflfastR")
+  attr(stats_df_week, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # COMBINE SEASON LEVEL STATS #################################################
+
+  stats_df_season <-
+    purrr::map(
+      seq(1999, nflreadr::most_recent_season()),
+      ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_def_season_{.x}.qs"))
+    ) |>
+    purrr::list_rbind()
+
+  attr(stats_df_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    data_frame = stats_df,
+    data_frame = stats_df_week,
     file_name = "player_stats_def",
     nflverse_type = "player stats: defense",
+    release_tag = "player_stats",
+    file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = stats_df_season,
+    file_name = "player_stats_def_season",
+    nflverse_type = "player stats: defense (season)",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
   )
@@ -98,15 +242,62 @@ release_playerstats_defense_combined <- function(...){
 release_playerstats_kicking <- function(season) {
   cli::cli_progress_step("Starting {.fct nflfastR::calculate_player_stats_kicking} for {season}!")
 
-  ps <- nflreadr::load_pbp(season) |>
-    nflfastR::calculate_player_stats_kicking(weekly = TRUE)
+  pbp <- nflreadr::load_pbp(season)
 
-  attr(ps, "nflfastR_version") <- packageVersion("nflfastR")
+  # WEEK LEVEL SUMMARY #########################################################
+
+  ps_weekly <- pbp |>
+    nflfastR::calculate_player_stats_kicking(weekly = TRUE)
+  attr(ps_weekly, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # SEASON LEVEL SUMMARY #######################################################
+
+  ps_reg_season <- pbp |>
+    dplyr::filter(season_type == "REG") |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_post_season <- pbp |>
+    dplyr::filter(season_type == "POST") |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_all_season <- pbp |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season = season,
+      season_type = "REG+POST"
+    ) |>
+    dplyr::select(season, season_type, dplyr::everything())
+
+  ps_season <- dplyr::bind_rows(ps_reg_season, ps_post_season, ps_all_season)
+
+  attr(ps_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    ps,
+    ps_weekly,
     file_name = glue::glue("player_stats_kicking_{season}"),
     nflverse_type = "player stats: kicking",
+    release_tag = "player_stats",
+    file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    ps_season,
+    file_name = glue::glue("player_stats_kicking_season_{season}"),
+    nflverse_type = "player stats: kicking (season)",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
   )
@@ -117,19 +308,44 @@ release_playerstats_kicking <- function(season) {
 
 release_playerstats_kicking_combined <- function(...){
   cli::cli_progress_step("Release combined kicking stats...")
-  stats_df <-
+
+  # COMBINE WEEK LEVEL STATS ###################################################
+
+  stats_df_week <-
     purrr::map(
       seq(1999, nflreadr::most_recent_season()),
       ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_kicking_{.x}.qs"))
     ) |>
     purrr::list_rbind()
 
-  attr(stats_df, "nflfastR_version") <- packageVersion("nflfastR")
+  attr(stats_df_week, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # COMBINE SEASON LEVEL STATS #################################################
+
+  stats_df_season <-
+    purrr::map(
+      seq(1999, nflreadr::most_recent_season()),
+      ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_kicking_season_{.x}.qs"))
+    ) |>
+    purrr::list_rbind()
+
+  attr(stats_df_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
-    data_frame = stats_df,
+    data_frame = stats_df_week,
     file_name =  "player_stats_kicking",
     nflverse_type = "player stats: kicking",
+    release_tag = "player_stats",
+    file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = stats_df_season,
+    file_name =  "player_stats_kicking_season",
+    nflverse_type = "player stats: kicking (season)",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
   )
