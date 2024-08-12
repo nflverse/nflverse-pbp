e973c324eb52ee3ce3474881bd861a442ca55f6c
Sebastian Carl <mrcaseb@gmail.com>
2024-08-12 16:41:16 +0200

    season level kicking stats

--- R/update_player_stats.R
+++ R/update_player_stats.R
@@ -230,15 +230,56 @@ release_playerstats_defense_combined <- function(...){
 release_playerstats_kicking <- function(season) {
   cli::cli_progress_step("Starting {.fct nflfastR::calculate_player_stats_kicking} for {season}!")
 
-  ps <- nflreadr::load_pbp(season) |>
-    nflfastR::calculate_player_stats_kicking(weekly = TRUE)
+  pbp <- nflreadr::load_pbp(season)
 
+  # WEEK LEVEL SUMMARY #########################################################
+
+  ps_weekly <- pbp |>
+    nflfastR::calculate_player_stats_kicking(weekly = TRUE)
   attr(ps, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # SEASON LEVEL SUMMARY #######################################################
+
+  ps_reg_season <- pbp |>
+    dplyr::filter(season_type == "REG") |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season_type = "REG"
+    )
+
+  ps_post_season <- pbp |>
+    dplyr::filter(season_type == "POST") |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season_type = "POST"
+    )
+
+  ps_all_season <- pbp |>
+    nflfastR::calculate_player_stats_kicking(weekly = FALSE) |>
+    dplyr::mutate(
+      season_type = "REG+POST"
+    )
+
+  ps_season <- dplyr::bind_rows(ps_reg_season, ps_post_season, ps_all_season)
+
+  attr(ps_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+
+  # RELEASE ####################################################################
 
   nflversedata::nflverse_save(
     ps,
     file_name = glue::glue("player_stats_kicking_{season}"),
     nflverse_type = "player stats: kicking",
+    release_tag = "player_stats",
+    file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    ps,
+    file_name = glue::glue("player_stats_kicking_season_{season}"),
+    nflverse_type = "player stats: kicking (season)",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
   )
@@ -249,7 +290,10 @@ release_playerstats_kicking <- function(season) {
 
 release_playerstats_kicking_combined <- function(...){
   cli::cli_progress_step("Release combined kicking stats...")
-  stats_df <-
+
+  # COMBINE WEEK LEVEL STATS ###################################################
+
+  stats_df_week <-
     purrr::map(
       seq(1999, nflreadr::most_recent_season()),
       ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_kicking_{.x}.qs"))
@@ -258,10 +302,32 @@ release_playerstats_kicking_combined <- function(...){
 
   attr(stats_df, "nflfastR_version") <- packageVersion("nflfastR")
 
+
+  # COMBINE SEASON LEVEL STATS #################################################
+
+  stats_df_season <-
+    purrr::map(
+      seq(1999, nflreadr::most_recent_season()),
+      ~ nflreadr::qs_from_url(glue::glue("https://github.com/nflverse/nflverse-data/releases/download/player_stats/player_stats_kicking_season_{.x}.qs"))
+    ) |>
+    purrr::list_rbind()
+
+  attr(stats_df_season, "nflfastR_version") <- packageVersion("nflfastR")
+
+  # RELEASE ####################################################################
+
   nflversedata::nflverse_save(
     data_frame = stats_df,
     file_name =  "player_stats_kicking",
     nflverse_type = "player stats: kicking",
+    release_tag = "player_stats",
+    file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
+  )
+
+  nflversedata::nflverse_save(
+    data_frame = stats_df_season,
+    file_name =  "player_stats_kicking_season",
+    nflverse_type = "player stats: kicking (season)",
     release_tag = "player_stats",
     file_types = c("rds", "csv", "parquet", "qs", "csv.gz")
   )
