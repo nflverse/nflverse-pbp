3df255fdddef124eb7da058747cd12b22ae9b737
mrcaseb <mrcaseb@gmail.com>
2025-10-01 10:04:12 +0200

    be more specific about NAs and make output a tibble

--- R/update_teams_colors_logos.R
+++ R/update_teams_colors_logos.R
@@ -4,17 +4,18 @@ cli::rule("Start teams data update process")
 released <- data.table::fread(paste0(
   "https://github.com/nflverse/nflverse-data/",
   "releases/download/teams/teams_colors_logos.csv"
-))
+), na.strings = "")
 
 # Load data in nflverse-pbp repo
-repo_data <- data.table::fread("teams_colors_logos.csv")
+repo_data <- data.table::fread("teams_colors_logos.csv", na.strings = "")
 
 # The workflow should only trigger if the data changed but it doesn't hurt
 # to make sure we only update the data if the comparison shows that something
 # really has changed
 if (!identical(released, repo_data)) {
   cli::cli_alert_info("Going to release updated teams data.")
-  out <- dplyr::rows_upsert(released, repo_data, by = "team_abbr")
+  out <- dplyr::rows_upsert(released, repo_data, by = "team_abbr") |>
+    dplyr::as_tibble()
   nflversedata::nflverse_save(
     data_frame = out,
     file_name = "teams_colors_logos",
